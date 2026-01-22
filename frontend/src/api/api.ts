ed7f394383b3eecd889c94b8552692422a8c8e38
MatijaBujanic <62101103+MatijaBujanic@users.noreply.github.com>
2026-01-22 08:05:10 +0100

    Revert "Axios endpoint"
    This reverts commit 64a35dbe6ea377a0e251764ec690b9594842010b.

--- frontend/src/api/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://localhost:8080",
-  withCredentials: true,
-  headers: {
-    Accept: "application/json",
-  },
-});
--- frontend/src/types/Quiz.ts
+++ frontend/src/types/Quiz.ts
@@ -1,22 +1,9 @@
 export type Quiz = {
-  date: string;
-  time: {
-    hour: number;
-    minute: number;
-    second: number;
-    nano: number;
-  };
+  id: number;
+  title: string;
   description: string;
-  status: string;
-  quiz_id: number;
-  quiz_name: string;
-  quiz_theme: string;
-  application_type: string;
-  number_of_rounds: number;
-  max_points: number;
-  created_at: string;
-  organizer_id: number;
-  location_id: number;
-  average_rating: number | null;
-  rating_count: number;
+  date: string;
+  location: string;
+  maxTeams: number;
+  registeredTeams: number;
 };
